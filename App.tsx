0b3ead31ef348bded329a6b48e0353af414eb76b
Daniel Whiffing <danielwhiffing@gmail.com>
2022-10-07 19:36:33 -0400

    Adds font, icons, and launch screen to ios

--- App.tsx
+++ App.tsx
@@ -1,6 +1,23 @@
 import * as React from 'react'
 import Routes from './src/components/Routes'
+import {
+  MD3LightTheme as DefaultTheme,
+  Provider as PaperProvider,
+} from 'react-native-paper'
 
-const App = () => <Routes />
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    secondary: 'yellow',
+  },
+}
+
+const App = () => (
+  <PaperProvider theme={theme}>
+    <Routes />
+  </PaperProvider>
+)
 
 export default App
